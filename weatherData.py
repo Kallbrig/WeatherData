a3fae4486826bc590f3463c67833a974f41105f4
Kristopher Allbright <kallbright@troy.edu>
2020-03-29 15:54:44 -0500

    changed default location.

--- weatherData.py
+++ weatherData.py
@@ -1,26 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
-from datetime import datetime
+
 
 from datetime import datetime
 
 day = datetime.today().strftime('%Y%m%d')
 
-page = requests.get("https://forecast.weather.gov/MapClick.php?lon=-85.96042156219481&lat=31.78950142221656")
+# REPLACE THESE WITH YOUR OWN.
+# DEFAULTS ARE NYC
+lat = '40.758896'
+long = '-73.985130'
+
+page = requests.get("https://forecast.weather.gov/MapClick.php?lon=" + long + "&lat=" + lat)
 
 soup = BeautifulSoup(page.content, 'html.parser')
 week = soup.find(id='seven-day-forecast-body')
 items = (week.find_all(class_='tombstone-container'))
 curent_cond = soup.find(id='current-conditions-body')
-
 cur_temp = curent_cond.find(class_='myforecast-current-lrg').get_text()
 cur_forecast = curent_cond.find(class_="myforecast-current").get_text()
-print(cur_temp)
-print(cur_forecast)
 
-line = {'day': day,
+
+info = {'day': day,
         'condition': cur_forecast,
         'temp': cur_temp, }
 
-print(line)
+for entry in info:
+    print(info[entry])
